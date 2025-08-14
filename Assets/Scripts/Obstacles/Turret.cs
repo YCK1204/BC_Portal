8793de37b16c2b0c7e8fd02beeefbd1f6d50ca1b
SeungYeop Oh <sadrain9274@naver.com>
2025-08-14 15:48:05 +0900

    [feat] Turret 스크립트 필드 작성(플레이어 추적)

--- Assets/Scripts/Obstacles/Turret.cs
+++ Assets/Scripts/Obstacles/Turret.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class Turret : ObstacleBase
 {
+    [Header("Turret Parts")]
+    [SerializeField] private Transform turretBase; // 고정대
+    [SerializeField] private Transform turretHead; // 본체
+
     [Header("Turret Settings")]
-    [SerializeField] private float rotationSpeed = 5f;
-    [SerializeField] private float detectionSpeed = 5f;
+    [SerializeField] private float rotationSpeed = 5f; // 터렛 회전 속도
+    [SerializeField] private float detectionSpeed = 5f; // 터렛 감지 속도
+    [SerializeField] private Transform target; // 타겟(플레이어)
 
 }

2f9623302308e9bb2dc5df620df68f77cb03265f
SeungYeop Oh <sadrain9274@naver.com>
2025-08-15 19:26:15 +0900

    [feat] 레이저 포인터 LineRenderer 설정 추가

--- Assets/Scripts/Obstacles/LaserPointer.cs
+++ Assets/Scripts/Obstacles/LaserPointer.cs
@@ -14,5 +14,38 @@ public class LaserPointer : MonoBehaviour
     void Start()
     {
         lineRenderer = GetComponentInChildren<LineRenderer>();
-        lineRenderer.positionCount = 2;
+        lineRenderer.positionCount = 2; // 레이저 포인터 점 갯수
     }
+
+    void Update()
+    {
+        if(target != null)
+        {
+            OnLazer(startPoint.position, target.position);
+        }
+        else
+        {
+            Vector3 endPoint = startPoint.position + startPoint.forward * laserLength;
+            OnLazer(startPoint.position, endPoint);
+        }        
+    }
+
+    public void OnLazer(Vector3 from, Vector3 to) // 레이저 작동
+    {
+        lineRenderer.enabled = true;
+        lineRenderer.SetPosition(0, from);
+        lineRenderer.SetPosition(1, to);
+    }
+
+
+    public void SetTarget(Transform newTarget)
+    {
+        target = newTarget;
+    }
+
+    public void ClearTarget()
+    {
+        target = null;
+    }
+
+}

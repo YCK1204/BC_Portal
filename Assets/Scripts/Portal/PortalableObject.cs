62a7c0fefcc2a435b15857878c7936e01c8f4288
Hira7388 <mine7388@gmail.com>
2025-08-18 19:10:30 +0900

    [Feat] 포탈 생성 로직 구현
    다만 아직 플레이어가 포탈을 발사하는 로직 추가는 X

--- Assets/Scripts/Portal/PortalableObject.cs
+++ Assets/Scripts/Portal/PortalableObject.cs
@@ -20,6 +20,13 @@ public class PortalableObject : MonoBehaviour
 
     private static readonly Quaternion halfTurn = Quaternion.Euler(0f, 180.0f, 0f);
 
+    protected virtual void Awake()
+    {
+        // 스크립트가 시작될 때 자신의 컴포넌트를 찾아 변수에 할당합니다.
+        _rigidbody = GetComponent<Rigidbody>();
+        _collider = GetComponent<Collider>();
+    }
+
     // 포탈에 들어갈 때 실행하는 메서드
     // 포탈 뒤 벽 콜리전을 무시하도록 해야 한다.
     public void SetIsInPortal(Portal inPortal, Portal outPortal, Collider wallCollider)
@@ -42,25 +49,34 @@ public class PortalableObject : MonoBehaviour
     public virtual void Warp()
     {
         // 포탈의 트랜스 폼을 가져온다.
-        var inTransform = _inPortal.transform;
-        var outTransform = _outPortal.transform;
+        var inTransform = _inPortal.transform; // 입구 포탈
+        var outTransform = _outPortal.transform; // 출구 포탈
 
-        // 위치 변환
+        // ==== 위치 변환
+        // 나의 좌표를 월드 좌표에서 입구 포탈의 로컬 좌표로 변환한다.
         Vector3 relativePos = inTransform.InverseTransformPoint(transform.position);
+        // 180도 회전한다. (입구 포탈의 "앞"이 출구 포탈의 "뒤" 이다.
         relativePos = halfTurn * relativePos;
+        // portalable 오브젝트를 위에서 계산한 로컬 좌표에서 월드 좌표로 변환한다.
         transform.position = outTransform.TransformPoint(relativePos);
 
-        // 회전 변환
+        // ==== 회전 변환
+        // 나의 회전값을 월드 회전값에서 입구 포탈의 회전값으로 변환한다.
         Quaternion relativeRot = Quaternion.Inverse(inTransform.rotation) * transform.rotation;
+        // 위치와 동일하게 180도 회전한다.
         relativeRot = halfTurn * relativeRot;
+        // 나의 위치를 위에서 계산한 로컬 회전값에서 월드 회전값으로 변환한다.
         transform.rotation = outTransform.rotation * relativeRot;
 
-        // 속도 변환 (운동량 보존)
+        // ==== 속도 변환 (운동량 보존)
+        // 나의 방향을 월드 좌표에서 입구 포탈의 로컬 방향으로 변환한다.
         Vector3 relativeVel = inTransform.InverseTransformDirection(_rigidbody.velocity);
+        // 또 동일하게 180도 회전한다.
         relativeVel = halfTurn * relativeVel;
+        // 나의 방향을 위에서 계산한 로컬 방향에서 월드 방향으로 변환한다.
         _rigidbody.velocity = outTransform.TransformDirection(relativeVel);
 
-        // 포탈 참조 교환
+        // ==== 포탈 참조 교환
         var tmp = _inPortal;
         _inPortal = _outPortal;
         _outPortal = tmp;

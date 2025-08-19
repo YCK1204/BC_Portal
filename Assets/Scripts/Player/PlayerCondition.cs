a542dfdd8321537e979d0ac91024122f4467dd09
smjawhn <84219257+smjawhn@users.noreply.github.com>
2025-08-19 22:45:26 +0900

    [Feat] Health기능 추가

---
+++ Assets/Scripts/Player/PlayerCondition.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCondition : MonoBehaviour
+{
+    public Condition health;
+    // Start is called before the first frame update
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        health.Add(health.passiveValue * Time.deltaTime);
+
+        if(health.curValue <= 0f)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        Debug.Log("Die");
+    }
+}

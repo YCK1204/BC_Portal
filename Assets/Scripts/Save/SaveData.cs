dfcc6c900c5f3ef5986268198adb348433484760
Hira7388 <mine7388@gmail.com>
2025-08-22 10:04:35 +0900

    [ReFactor] 불필요한 코드 정리

--- Assets/Scripts/Save/SaveData.cs
+++ Assets/Scripts/Save/SaveData.cs
@@ -6,20 +6,11 @@ using UnityEngine;
 [System.Serializable]
 public class SaveData
 {
-    // 저장할 데이터 목록
-
     // ======= 스테이지 정보 =======
     // 클리어한 모든 스테이지 목록   
     public List<int> clearedStagesIndex;
     // 마지막으로 클리어한 스테이지 인덱스
     public int lastClearStageIndex;
-
-    // ======= 플레이어 정보 =======
-    // 플레이어 정보는 불러올 필요 없습니다.
-    //public Vector3 playerRotation; //JsonUtility는 Qauternion을 저장하지 못해서 transform.eulerAngles을 저장해야 합니다.
-    //public Vector3 playerPosition;
-
-    // ======= 추가 =======
 
 
     // 새 게임 시작 시 생성자
@@ -27,8 +18,5 @@ public class SaveData
     {
         this.clearedStagesIndex = new List<int>();
         this.lastClearStageIndex = 0;
-
-        //this.playerRotation = Vector3.zero;
-        //this.playerPosition = Vector3.zero;
     }
 }
